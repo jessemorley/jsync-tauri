95ea19108f2b9ebfdd1dc474d5d26fa367a405dc
jessemorley <jessmorley@gmail.com>
2025-12-27 15:54:52 +1100

    Disable default context menu and bump version to v0.1.2

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./App.css";
 
+// Disable context menu in production to prevent 'Reload' and other browser defaults
+if (window.location.hostname !== 'localhost') {
+  document.addEventListener('contextmenu', (e) => e.preventDefault());
+}
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
